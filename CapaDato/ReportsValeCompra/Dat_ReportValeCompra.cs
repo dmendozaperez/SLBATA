62bb9d3f79c6c3ee469753c1d7544ee103f758d5
Junior Mallqui <Junior Mallqui@DESKTOP-PQNTBIV>
2018-03-12 10:21:04 -0500

    ReporteValeCompra

---
+++ CapaDato/ReportsValeCompra/Dat_ReportValeCompra.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using CapaEntidad.ReportsValeCompra;
+using CapaEntidad.Util;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDato.ReportsValeCompra
+{
+    public class Dat_ReportValeCompra
+    {
+        public List<Reporte_Resultado> listarReporte(Reporte_Filtro filtro)  
+        {
+            string sqlquery = "USP_ListarValesCompra";
+            List<Reporte_Resultado> lista = null;
+            try
+            {
+                using (SqlConnection cn = new SqlConnection(Ent_Conexion.conexion))
+                {
+                    using (SqlCommand cmd = new SqlCommand(sqlquery, cn))
+                    {
+
+
+                        SqlParameter oEstado = cmd.Parameters.Add("@Estado", SqlDbType.VarChar);
+                        oEstado.Direction = ParameterDirection.Input;
+                        oEstado.Value = filtro.report_Estado;
+
+                        SqlParameter ostrRuc = cmd.Parameters.Add("@strRucCliente", SqlDbType.VarChar);
+                        ostrRuc.Direction = ParameterDirection.Input;
+                        ostrRuc.Value = filtro.report_listRuc;
+
+
+                        cmd.CommandTimeout = 0;
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        using (SqlDataAdapter da = new SqlDataAdapter(cmd))
+                        {
+                            DataTable dt = new DataTable();
+                            da.Fill(dt);
+                            lista = new List<Reporte_Resultado>();
+                            lista = (from DataRow dr in dt.Rows
+                                     select new Reporte_Resultado()
+                                     {
+                                       
+                                         rep_dni = dr["rep_dni"].ToString(),
+                                         rep_nombre = dr["rep_nombre"].ToString(),
+                                         rep_apellidoPater = dr["rep_apellidoPater"].ToString(),
+                                         rep_apellidoMater = dr["rep_apellidoMater"].ToString(),
+                                         rep_email = dr["rep_email"].ToString(),
+
+                                         rep_CupBarra = dr["rep_CupBarra"].ToString(),
+                                         rep_CupNumero = dr["rep_CupNumero"].ToString(),
+                                         rep_CupMonto = dr["rep_CupMonto"].ToString(),
+                                         rep_CupEstado = dr["rep_CupEstado"].ToString(),
+
+                                         rep_docTipo = dr["rep_docTipo"].ToString(),
+                                         rep_docSerie = dr["rep_docSerie"].ToString(),
+                                         rep_docNro = dr["rep_docNro"].ToString(),
+                                         rep_docfecha = dr["rep_docfecha"].ToString(),
+
+                                         rep_tdaCodigo = dr["rep_tdaCodigo"].ToString(),
+                                         rep_tdaDesc = dr["rep_tdaDesc"].ToString(),
+                                         rep_tdaDirec = dr["rep_tdaDirec"].ToString(),
+
+
+                                     }).ToList();
+
+                        }
+                    }
+                }
+            }
+            catch (Exception exc)
+            {
+
+                lista = null;
+            }
+            return lista;
+        }
+
+    }
+}
---
+++ CapaPresentacion/Controllers/ReportsValeCompraController.cs
@@ -0,0 +1,95 @@
+﻿using CapaDato.ValeCompra;
+using CapaDato.ReportsValeCompra;
+using CapaEntidad.ValeCompra;
+using CapaEntidad.ReportsValeCompra;
+using CapaEntidad.Control;
+using CapaEntidad.Menu;
+using CapaEntidad.Util;
+using CapaPresentacion.Bll;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+using System.IO;
+using System.IO.Compression;
+using System.Text;
+
+namespace CapaPresentacion.Controllers
+{
+    public class ReportsValeCompraController : Controller
+    {
+        // GET: ReportsValeCompra
+        private Dat_ValeCompra datvalecompra = new Dat_ValeCompra();
+        private Dat_ReportValeCompra datReportvalecompra = new Dat_ReportValeCompra();
+        private Dat_Cliente datCliente = new Dat_Cliente();
+        private string _session_listValeCompraDetalle_private = "_session_listValeCompraDetalle_private";
+
+        public ActionResult Index()
+        {
+            Ent_Usuario _usuario = (Ent_Usuario)Session[Ent_Constantes.NameSessionUser];
+            string actionName = this.ControllerContext.RouteData.GetRequiredString("action");
+            string controllerName = this.ControllerContext.RouteData.GetRequiredString("controller");
+            string return_view = actionName + "|" + controllerName;
+
+            if (_usuario == null)
+            {
+                return RedirectToAction("Login", "Control", new { returnUrl = return_view });
+            }
+            else
+            {
+                #region<VALIDACION DE ROLES DE USUARIO>
+                Boolean valida_rol = true;
+                Basico valida_controller = new Basico();
+                List<Ent_Menu_Items> menu = (List<Ent_Menu_Items>)Session[Ent_Global._session_menu_user];
+                valida_rol = valida_controller.AccesoMenu(menu, this);
+                #endregion
+                if (valida_rol)
+                {
+                    ViewBag.cliente = datCliente.get_lista();
+                    return View(lista("",""));
+                }
+                else
+                {
+                    return RedirectToAction("Login", "Control", new { returnUrl = return_view });
+                }
+            }
+        }
+
+        public PartialViewResult ListaValeCompra(string []dwcliente, string  estado)
+        {
+            Reporte_Filtro filtro = new Reporte_Filtro();
+            string strListRuc = "";
+            if(estado == null)
+                estado = "";
+
+            if (dwcliente == null)
+                strListRuc = "";
+            else {
+
+                for (int i = 0; i < dwcliente.Length; i++) {
+                    strListRuc = strListRuc + ',' + dwcliente[i];
+                }
+                strListRuc = strListRuc.Substring(1);
+
+            }
+
+            filtro.report_Estado = estado;
+            filtro.report_listRuc = "";
+
+            return PartialView(lista(strListRuc, estado));
+        }
+
+        public List<Reporte_Resultado> lista(string rucCliente, string estado)//(Reporte_Filtro filtro)
+        {
+            Reporte_Filtro filtro =  new Reporte_Filtro();
+            filtro.report_listRuc = rucCliente;
+            filtro.report_Estado = estado;
+            List<Reporte_Resultado> list = datReportvalecompra.listarReporte(filtro);
+            Session[_session_listValeCompraDetalle_private] = list;
+            return list;
+        }
+    }
+}
