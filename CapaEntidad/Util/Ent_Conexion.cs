2dcc8786fcbbeeaf41aff5ee83be0ba9a476d342
Junior Mallqui <Junior Mallqui@DESKTOP-PQNTBIV>
2018-03-13 14:44:40 -0500

    Cambio_español_boostrap

--- CapaEntidad/Util/Ent_Conexion.cs
+++ CapaEntidad/Util/Ent_Conexion.cs
@@ -12,7 +12,7 @@ namespace CapaEntidad.Util
         {
             get {
                 //  return "Server=des.bgr.pe;Database=BdTiendaReplica;User ID=junior;Password=Bata2018**@;Trusted_Connection=False;";
-                return "Server=10.10.10.208;Database=BdTiendaReplica;User ID=sa;Password=Bata2013;Trusted_Connection=False;";
+                return "Server=10.10.10.208;Database=BdTienda;User ID=sa;Password=Bata2013;Trusted_Connection=False;";
 
             }
         }
