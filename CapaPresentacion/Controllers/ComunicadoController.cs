a737dfdabd6f517cac03a5de3fcb145d31afb273
David Mendoza Perez <david_mendozap@hotmail.com>
2019-01-10 08:12:07 -0500

    insercion de devexprees file manager

---
+++ CapaPresentacion/Controllers/ComunicadoController.cs
@@ -0,0 +1,36 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapaPresentacion.Controllers
+{
+    public class ComunicadoController : Controller
+    {
+        // GET: Comunicado
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [ValidateInput(false)]
+        public ActionResult FileManagerPartial()
+        {
+            return PartialView("_FileManagerPartial", ComunicadoControllerFileManagerSettings.Model);
+        }
+
+        public FileStreamResult FileManagerPartialDownload()
+        {
+            return FileManagerExtension.DownloadFiles("FileManager", ComunicadoControllerFileManagerSettings.Model);
+        }
+    }
+    public class ComunicadoControllerFileManagerSettings
+    {
+        public const string RootFolder = @"~\Files\Comunicado";
+
+        public static string Model { get { return RootFolder; } }
+    }
+
+}
