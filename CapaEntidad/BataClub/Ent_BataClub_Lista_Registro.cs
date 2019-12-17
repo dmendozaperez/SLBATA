7f670be36ed0fb01a74df9a30d3164e917b9ab0c
David Mendoza Perez <david_mendozap@hotmail.com>
2019-12-17 14:48:46 -0500

    BataClub Clientes

---
+++ CapaEntidad/BataClub/Ent_BataClub_Lista_Registro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad.BataClub
+{
+    public class Ent_BataClub_Lista_Registro
+    {
+        public string canal { get; set; }
+        public string tienda { get; set; }
+        public string dni { get; set; }
+        public string primer_nombre { get; set; }
+        public string segundo_nombre { get; set; }
+        public string apellido_pat { get; set; }
+        public string apellido_mat { get; set; }
+        public string genero { get; set; }
+        public string correo { get; set; }
+        public DateTime? fec_nac { get; set; }
+        public string telefono { get; set; }
+        public string ubicacion { get; set; }
+        public DateTime? fec_registro { get; set; }
+        public DateTime? miem_bataclub_fecha { get; set; }
+        public string miem_bataclub { get; set; }
+
+    }
+}
