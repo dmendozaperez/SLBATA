afa6b94f6f28b5a81cefd7c0c0b0a6e8cf680832
David Mendoza Perez <david_mendozap@hotmail.com>
2020-08-19 17:16:17 -0500

    Reportes Sac 1.0

---
+++ CapaEntidad/Bata/Ent_Bata_Sac.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidad.Bata
+{
+    public class Ent_Bata_Sac
+    {
+        
+        public List<Ent_Bata_Sac_Cliente> Bata_Sac_Cliente { get; set; }
+        public List<Ent_Bata_Sac_Cupones> Bata_Sac_Cupones { get; set; }
+        public List<Ent_Bata_Sac_Venta>   Bata_Sac_Venta { get; set; }
+
+    }
+    public class Ent_Bata_Sac_Cliente
+    {
+        public string dni { get; set; }
+        public string nombre { get; set; }
+        public string correo { get; set; }
+        public string telefono { get; set; }
+        public string ubicacion { get; set; }
+        public string fec_nac { get; set; }
+        public string bataclub { get; set; }
+    }
+    public class Ent_Bata_Sac_Cupones
+    {
+        public string barra { get; set; }
+        public string promocion { get; set; }
+        public string fecha_expiracion { get; set; }
+        
+    }
+    public class Ent_Bata_Sac_Venta
+    {
+        public string cod_tda { get; set; }
+        public string canal { get; set; }
+        public string tienda { get; set; }
+        public string tipodoc { get; set; }
+        public string numdoc { get; set; }
+        public string fecha { get; set; }
+        public string estado { get; set; }
+        public string pedido { get; set; }
+        public string fc_suna { get; set; }
+        public string fc_sfac { get; set; }
+        public string fc_nfac { get; set; }
+        public string fc_nint { get; set; }
+
+    }
+
+    public class Ent_Bata_Sac_Venta_Detalle
+    {
+        public string articulo { get; set; }
+        public string talla { get; set; }
+        public Int32 cantidad { get; set; }
+        public Decimal precio { get; set; }
+        public Decimal descuento { get; set; }
+        public Decimal total_linea { get; set; }
+        public string promocion { get; set; }
+
+    }
+
+}
--- CapaPresentacion/Controllers/BataController.cs
+++ CapaPresentacion/Controllers/BataController.cs
@@ -229,5 +229,290 @@ namespace CapaPresentacion.Controllers
                 aaData = result
             }, JsonRequestBehavior.AllowGet);
         }
+
+        #region<REGION DE REPORTE SAC>
+        public ActionResult BataSac()
+        {
+            Ent_Usuario _usuario = (Ent_Usuario)Session[Ent_Constantes.NameSessionUser];
+            string actionName = this.ControllerContext.RouteData.GetRequiredString("action");
+            string controllerName = this.ControllerContext.RouteData.GetRequiredString("controller");
+            string return_view = actionName + "|" + controllerName;
+
+            if (_usuario == null)
+            {
+                return RedirectToAction("Login", "Control", new { returnUrl = return_view });
+            }
+            else
+            {
+                Session[_session_sac_cupones] = null;
+                Session[_session_sac_venta] = null;
+                return View();
+            }
+
+         
+        }
+        private string _session_sac_cupones = "_session_sac_cupones";
+        private string _session_sac_venta = "_session_sac_venta";
+        private string _session_sac_venta_detalle = "_session_sac_venta_detalle";
+        public ActionResult get_batasac(string dni)
+        {
+            string mensaje = "";
+            string estado = "0";
+            try
+            {
+                Session[_session_sac_cupones] = null;
+                Session[_session_sac_venta] = null;
+                Session[_session_sac_venta_detalle] = null;
+
+                Ent_Bata_Sac batasac =lista_batasac(dni);
+                List<Ent_Bata_Sac_Cliente> sac_cliente = batasac.Bata_Sac_Cliente;
+                List<Ent_Bata_Sac_Cupones> sac_cupones = batasac.Bata_Sac_Cupones;
+                List<Ent_Bata_Sac_Venta> sac_venta = batasac.Bata_Sac_Venta;
+
+                if (sac_cliente.Count==0)
+                {
+                    mensaje = "El Numero de dni no existe en nuestra base de datos";
+
+                }
+                else
+                {
+                    estado = "1";
+                    Session[_session_sac_cupones] = sac_cupones;
+                    Session[_session_sac_venta] = sac_venta;
+                }                                
+                return Json(new { estado = estado, mensaje = mensaje, sac_cliente= sac_cliente, sac_cupones= sac_cupones });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { estado = "-1", mensaje = ex.Message });
+            }
+
+        }
+
+        public ActionResult get_venta_detalle(string canal, string cod_tda, string fc_nint, string numdoc, string pedido)
+        {
+            Session[_session_sac_venta_detalle] = null;
+            List<Ent_Bata_Sac_Venta_Detalle> listar_venta_det = lista_venta_detalle(canal,cod_tda,fc_nint,numdoc,pedido);
+
+            Session[_session_sac_venta_detalle] = listar_venta_det;
+
+            ViewBag.total = listar_venta_det.Sum(s=>s.total_linea);
+
+            return View();
+            //if (listar_venta_det.Count > 0)
+            //{
+            //    return View();// Json(new { estado = "1", mensaje = "" });
+            //}
+            //else
+            //{
+            //    return Json(new { estado = "0", mensaje = "No hay registros para mostrar" });
+            //}
+
+        }
+        public Ent_Bata_Sac lista_batasac(string dni)
+        {
+            Dat_Bata_Sac data_sac = null;
+            Ent_Bata_Sac lista_sac = null;
+            try
+            {
+
+                data_sac = new Dat_Bata_Sac();
+                lista_sac = data_sac.Get_Bata_Sac(dni);
+            }
+            catch (Exception)
+            {
+
+                lista_sac = new Ent_Bata_Sac();
+            }
+            return lista_sac;
+        }
+
+        public List<Ent_Bata_Sac_Venta_Detalle> lista_venta_detalle(string canal,string cod_tda,string fc_nint,string numdoc,string pedido)
+        {            
+            List<Ent_Bata_Sac_Venta_Detalle> listar = null;
+            Dat_Bata_Sac data_sac = null;
+            try
+            {
+
+                data_sac = new Dat_Bata_Sac();
+                listar = data_sac.Get_Venta_detalle(canal,cod_tda,fc_nint,numdoc,pedido);
+            }
+            catch (Exception)
+            {
+
+                listar = new List<Ent_Bata_Sac_Venta_Detalle>();
+            }
+            return listar;
+        }
+
+        public ActionResult getTableBataSacVentaAjax(Ent_jQueryDataTableParams param)
+        {
+            /*verificar si esta null*/
+            if (Session[_session_sac_venta] == null)
+            {
+                List<Ent_Bata_Sac_Venta> listdoc = new List<Ent_Bata_Sac_Venta>();
+                Session[_session_sac_venta] = listdoc;
+            }
+            //if (!String.IsNullOrEmpty(dniEliminar))
+            //{
+            //    List<Ent_BataClub_Cupones> listAct = (List<Ent_BataClub_Cupones>)(Session[_session_lista_clientes_cupon]);
+            //    listAct.Remove(listAct.Where(w => w.dniCliente == dniEliminar).FirstOrDefault());
+            //    Session[_session_lista_clientes_cupon] = listAct;
+            //}
+            //Traer registros
+            IQueryable<Ent_Bata_Sac_Venta> membercol = ((List<Ent_Bata_Sac_Venta>)(Session[_session_sac_venta])).AsQueryable();
+
+            //Manejador de filtros
+            int totalCount = membercol.Count();
+
+            IEnumerable<Ent_Bata_Sac_Venta> filteredMembers = membercol;
+
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                filteredMembers = membercol
+                    .Where(m =>
+                    m.tienda.ToUpper().Contains(param.sSearch.ToUpper()) ||
+                    m.numdoc.ToUpper().Contains(param.sSearch.ToUpper()) ||
+                    m.pedido.ToUpper().Contains(param.sSearch.ToUpper())
+                    );
+            }
+
+            //Manejador de orden
+            var displayMembers = filteredMembers
+                .Skip(param.iDisplayStart)
+                .Take(param.iDisplayLength);
+
+            var result = from a in displayMembers
+                         select new
+                         {
+                             a.canal,
+                             a.tienda,
+                             a.tipodoc,
+                             a.numdoc,
+                             a.fecha,
+                             a.estado,
+                             a.pedido,
+                             a.fc_nint,
+                             a.cod_tda
+                         };
+            //Se devuelven los resultados por json
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = totalCount,
+                iTotalDisplayRecords = filteredMembers.Count(),
+                aaData = result
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult getTableBataSacCuponAjax(Ent_jQueryDataTableParams param)
+        {
+            /*verificar si esta null*/
+            if (Session[_session_sac_cupones] == null)
+            {
+                List<Ent_Bata_Sac_Cupones> listdoc = new List<Ent_Bata_Sac_Cupones>();
+                Session[_session_sac_cupones] = listdoc;
+            }
+            //if (!String.IsNullOrEmpty(dniEliminar))
+            //{
+            //    List<Ent_BataClub_Cupones> listAct = (List<Ent_BataClub_Cupones>)(Session[_session_lista_clientes_cupon]);
+            //    listAct.Remove(listAct.Where(w => w.dniCliente == dniEliminar).FirstOrDefault());
+            //    Session[_session_lista_clientes_cupon] = listAct;
+            //}
+            //Traer registros
+            IQueryable<Ent_Bata_Sac_Cupones> membercol = ((List<Ent_Bata_Sac_Cupones>)(Session[_session_sac_cupones])).AsQueryable();
+
+            //Manejador de filtros
+            int totalCount = membercol.Count();
+
+            IEnumerable<Ent_Bata_Sac_Cupones> filteredMembers = membercol;
+
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                filteredMembers = membercol
+                    .Where(m =>
+                    m.barra.ToUpper().Contains(param.sSearch.ToUpper()) 
+                    );
+            }
+
+            //Manejador de orden
+            var displayMembers = filteredMembers
+                .Skip(param.iDisplayStart)
+                .Take(param.iDisplayLength);
+
+            var result = from a in displayMembers
+                         select new
+                         {
+                             a.barra,
+                             a.promocion,
+                             a.fecha_expiracion,                             
+                         };
+            //Se devuelven los resultados por json
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = totalCount,
+                iTotalDisplayRecords = filteredMembers.Count(),
+                aaData = result
+            }, JsonRequestBehavior.AllowGet);
+        }
+        public ActionResult getTableBataSacVentaDetAjax(Ent_jQueryDataTableParams param)
+        {
+            /*verificar si esta null*/
+            if (Session[_session_sac_venta_detalle] == null)
+            {
+                List<Ent_Bata_Sac_Venta_Detalle> listdoc = new List<Ent_Bata_Sac_Venta_Detalle>();
+                Session[_session_sac_venta_detalle] = listdoc;
+            }
+            //if (!String.IsNullOrEmpty(dniEliminar))
+            //{
+            //    List<Ent_BataClub_Cupones> listAct = (List<Ent_BataClub_Cupones>)(Session[_session_lista_clientes_cupon]);
+            //    listAct.Remove(listAct.Where(w => w.dniCliente == dniEliminar).FirstOrDefault());
+            //    Session[_session_lista_clientes_cupon] = listAct;
+            //}
+            //Traer registros
+            IQueryable<Ent_Bata_Sac_Venta_Detalle> membercol = ((List<Ent_Bata_Sac_Venta_Detalle>)(Session[_session_sac_venta_detalle])).AsQueryable();
+
+            //Manejador de filtros
+            int totalCount = membercol.Count();
+
+            IEnumerable<Ent_Bata_Sac_Venta_Detalle> filteredMembers = membercol;
+
+            if (!string.IsNullOrEmpty(param.sSearch))
+            {
+                filteredMembers = membercol
+                    .Where(m =>
+                    m.articulo.ToUpper().Contains(param.sSearch.ToUpper())
+                    );
+            }
+
+            //Manejador de orden
+            var displayMembers = filteredMembers
+                .Skip(param.iDisplayStart)
+                .Take(param.iDisplayLength);
+
+            var result = from a in displayMembers
+                         select new
+                         {
+                             a.articulo,
+                             a.talla,
+                             a.cantidad,
+                             a.precio,
+                             a.descuento,
+                             a.total_linea,
+                             a.promocion,
+                         };
+            //Se devuelven los resultados por json
+            return Json(new
+            {
+                sEcho = param.sEcho,
+                iTotalRecords = totalCount,
+                iTotalDisplayRecords = filteredMembers.Count(),
+                aaData = result
+            }, JsonRequestBehavior.AllowGet);
+        }
+
+        #endregion
+
     }
 }
